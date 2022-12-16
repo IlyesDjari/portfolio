6a0e049c2e2186070b15dee6f405575011e09f67
IlyesDjari <71768687+IlyesDjari@users.noreply.github.com>
2022-12-16 21:58:00 +0100

    Added fade on image

--- port/src/components/home/subPages/Introduction.tsx
+++ port/src/components/home/subPages/Introduction.tsx
@@ -24,6 +24,7 @@ export default function Introduction() {
             
             <div className="imageEffect">
             <Link to="/about">
+                <Fade cascade damping={0.3} triggerOnce></Fade>
             <img className="ilyesPicture" src="/images/ilyesMono.png" alt="ilyes" />
             <img className="ilyesPicture" src="/images/ilyesMono.png" alt="ilyes" />
             <img className="ilyesPicture" src="/images/ilyesMono.png" alt="ilyes" />
